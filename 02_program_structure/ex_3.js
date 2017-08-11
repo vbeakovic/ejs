3e74bd17d53be1748a33648b7b05ad86bf71c9f0
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 20:31:57 +0200

    some functions

---
+++ 02_program_structure/ex_3.js
@@ -0,0 +1,11 @@
+//alert("Good morning!");
+
+var x = 30;
+console.log("the value of x is", x);
+
+console.log(Math.max(2, 4));
+
+console.log(Math.min(2, 4) + 100);
+
+confirm("Shall we, then?");
+prompt("Tell me everything you know.", "...");
