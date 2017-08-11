e47a1ed116d6cee130f77276f77ae40eb9e507cb
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 19:45:27 +0200

    strings

---
+++ 01_values_types_operators/ex_2.js
@@ -0,0 +1,9 @@
+// strings
+console.log("Patch my boat with a chewing gum");
+console.log("Monkeys wave goodbye");
+
+console.log("This is the first line\nAnd this is the second");
+
+console.log("A newline character is written like \"\\n\".");
+
+console.log("con" + "cat" + "e" + "nate");
