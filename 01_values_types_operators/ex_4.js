6de4141a03b5aef680bb15544263b3968f61ea4b
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 19:56:34 +0200

    booleans

---
+++ 01_values_types_operators/ex_4.js
@@ -0,0 +1,18 @@
+// boolean values
+console.log(3 > 2);
+console.log(3 < 2);
+
+console.log("Aardvark" < "Zoroaster");
+console.log("Itchy" != "Scratchy");
+console.log(NaN == NaN);
+
+console.log(true && false);
+console.log(true && true);
+
+console.log(false || true);
+console.log(false || false);
+
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
