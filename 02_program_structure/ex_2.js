dc28ba298be77b71c0d4896f90593dfa6d322e91
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 20:22:28 +0200

    variables

---
+++ 02_program_structure/ex_2.js
@@ -0,0 +1,20 @@
+var caught = 5 * 5;
+
+var ten = 10;
+
+console.log(ten * ten);
+
+
+var mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+
+var one = 1, two = 2;
+console.log(one + two);
