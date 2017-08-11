79afd5cb3efceb33af512a37630b276867c7c088
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 19:40:24 +0200

    numbers

---
+++ 01_values_types_operators/ex_1.js
@@ -0,0 +1,11 @@
+//numbers
+//whole number
+console.log(13);
+//fractional number
+console.log(9.81);
+//scientific notation
+console.log(2.998e8);
+
+// arithmetic
+console.log(100 + 4 * 11);
+console.log((100 + 4) * 11);
