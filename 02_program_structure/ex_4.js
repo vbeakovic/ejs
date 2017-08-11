4ee03cc255f18296ddc41156ec12ba7b30a03fb4
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 21:13:37 +0200

    flow control and comments

---
+++ 02_program_structure/ex_4.js
@@ -0,0 +1,95 @@
+var theNumber = Number(prompt("Pick a number", ""));
+alert("Your number is the square root of " + theNumber * theNumber);
+
+var theNumber = Number(prompt("Pick a number", ""));
+if (!isNaN(theNumber))
+  alert("Your number is the square root of " + theNumber * theNumber);
+
+
+var theNumber = Number(prompt("Pick a number", ""));
+if (!isNaN(theNumber))
+  alert("Your number is the square root of " + theNumber * theNumber);
+else
+  alert("Hey. Why didn't you give me a number?");
+
+
+var num = Number(prompt("Pick a number", "0"));
+
+if (num < 10)
+  alert("Small");
+else if (num < 100)
+  alert("Medium");
+else
+  alert("Large");
+
+
+console.log(0);
+console.log(2);
+console.log(4);
+console.log(6);
+console.log(8);
+console.log(10);
+console.log(12);
+
+
+var number = 0;
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+}
+
+var result = 1;
+var counter = 0;
+
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
+
+do {
+  var yourName = prompt("Who are you");
+} while (!yourName);
+console.log(yourName);
+
+
+for (var number = 0; number < 12; number = number + 2) {
+  console.log(number);
+}
+
+var result = 1;
+for (var counter = 0; counter < 10; counter = counter + 1) {
+  result = result * 2;
+}
+console.log(result);
+
+switch (prompt("What is the weather like?")) {
+  case "rainy":
+    console.log("Remeber to bring an umbrella");
+    break;
+  case "sunny":
+    console.log("Dress lightly");
+  case "cloudy":
+    console.log("Go outside");
+    break;
+  default:
+    console.log("Unknown weather type");
+}
+
+var accountBalance = calculateBalance(account);
+// It's a green hollow where a river sings
+accountBalance.adjust();
+// Madlly catching white tatters in the grass.
+var report = new Report();
+// Where the sun on the proud mounting rings:
+addToReport(accountBalance, report);
+// It's a little valley, foaming like light in a glass.
+
+/*
+ I first found this number scrawled on the back of one of
+ my notebooks a few years ago. Since then, it has often
+ dropped by, showing up in phone numbers and the serial
+ numbers of products that I've bought. It obviously likes
+ me, so I've decided to keep it.
+*/
+var myNumber = 11213;
