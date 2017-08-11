6308d1977e64661389d06839e3e5f7208edfb3f8
vbeakovic <valter.beakovic@gmail.com>
2017-08-11 20:04:45 +0200

    automatic type conversion

---
+++ 01_values_types_operators/ex_5.js
@@ -0,0 +1,9 @@
+// automatic type conversion
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == 0);
+
+console.log(null == undefined);
+console.log(null == 0);
