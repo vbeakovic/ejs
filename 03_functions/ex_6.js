b72bfdd7282a7e3edbf0a571ed7e92ec91b2062c
vbeakovic <valter.beakovic@gmail.com>
2017-08-12 21:25:14 +0200

    optional arguments

---
+++ 03_functions/ex_6.js
@@ -0,0 +1,15 @@
+console.log("Hello", "Good Evening", "How do you do?");
+
+function power(base, exponent) {
+  if (exponent == undefined) {
+    exponent = 2;
+  }
+  var result = 1;
+  for (var count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+
+console.log(power(4));
+console.log(power(4, 3));
