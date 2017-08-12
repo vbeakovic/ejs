517420d75ebfd62dfe4a9c1d0f1b5aa2e23a35a4
vbeakovic <valter.beakovic@gmail.com>
2017-08-12 20:48:45 +0200

    defining a function

---
+++ 03_functions/ex_1.js
@@ -0,0 +1,20 @@
+var square = function(x) {
+  return x * x;
+};
+
+console.log(square(12));
+
+var makeNoise = function () {
+  console.log("Pling!");
+}
+
+makeNoise();
+
+var power = function(base, exponent) {
+  var result = 1;
+  for (var count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+console.log(power(2, 10));
