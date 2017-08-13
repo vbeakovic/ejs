b1e8a2b7caf3b203f89696b89af389c9c0de1b73
vbeakovic <valter.beakovic@gmail.com>
2017-08-13 15:50:53 +0200

    arrays

--- 04_data_structures/ex_1.js
+++ 04_data_structures/ex_1.js
@@ -1,3 +1,16 @@
 var listOfNumbers = [2, 3, 5, 7, 11];
 console.log(listOfNumbers[2]);
 console.log(listOfNumbers[2 -1]);
+
+var doh = "Doh";
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+var mack = [];
+
+mack.push("Mack");
+mack.push("the", "Knife")
+console.log(mack);
+console.log(mack.join(" "));
+console.log(mack.pop());
+console.log(mack);
