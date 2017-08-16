bb00065d3476de3ece95629337008780f661f98f
vbeakovic <valter.beakovic@gmail.com>
2017-08-16 21:45:14 +0200

    computing correlations - modularized part 1

--- 04_data_structures/ex_5.js
+++ 04_data_structures/ex_5.js
@@ -1,25 +1,3 @@
-
-
-exports.phi = function(table) {
-  return (table [3] * table[0] - table[2] * table[1]) /
-  Math.sqrt((table[2] + table[3]) *
-            (table[0] + table[1]) *
-            (table[1] + table[3]) *
-            (table[0] + table[2]));
-}
-
-
-function hasEvent(event, entry) {
+exports.hasEvent = function(event, entry) {
   return entry.events.indexOf(event) != -1;
-}
-
-exports.tableFor = function(event, journal) {
-  var table = [0, 0, 0, 0];
-  for (var i = 0; i < journal.length; i++) {
-    var entry = journal[i], index = 0;
-    if (hasEvent(event, entry)) index += 1;
-    if (entry.squirrel) index += 2;
-    table[index] += 1;
-  }
-  return table;
 }
