13083fd1740d779cfe7c3efbf978f8827066a45f
vbeakovic <valter.beakovic@gmail.com>
2017-08-16 20:34:20 +0200

    computing correlations

--- 04_data_structures/ex_5.js
+++ 04_data_structures/ex_5.js
@@ -1,20 +1,20 @@
-const journal = require('./jacques_journal.js');
 
-function phi(table) {
+
+exports.phi = function(table) {
   return (table [3] * table[0] - table[2] * table[1]) /
   Math.sqrt((table[2] + table[3]) *
             (table[0] + table[1]) *
             (table[1] + table[3]) *
             (table[0] + table[2]));
 }
-console.log(phi([76, 9, 4, 1]));
 
 
-function hasEvent(event, entry) {
+
+exports.hasEvent = function(event, entry) {
   return entry.events.indexOf(event) != -1;
 }
 
-function tableFor(event, journal) {
+exports.tableFor = function(event, journal) {
   var table = [0, 0, 0, 0];
   for (var i = 0; i < journal.length; i++) {
     var entry = journal[i], index = 0;
@@ -24,4 +24,3 @@ function tableFor(event, journal) {
   }
   return table;
 }
-console.log(tableFor("pizza", journal));
