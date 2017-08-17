3a980d1d43a41549bf66013bea49ce2ebdf97d2d
vbeakovic <valter.beakovic@gmail.com>
2017-08-17 08:40:44 +0200

    strings and their properties

--- 04_data_structures/ex_9.js
+++ 04_data_structures/ex_9.js
@@ -1,3 +1,17 @@
 var myString = 'Fido';
 myString.myProperty = 'value';
 console.log(myString.myProperty);
+
+console.log('coconuts'.slice(4, 7));
+console.log('coconuts'.indexOf('u'));
+
+
+console.log('one two three'.indexOf('ee'));
+
+console.log(' okay \n '.trim());
+
+var string = 'abc';
+
+console.log(string.length);
+console.log(string.charAt(0));
+console.log(string[1]);
