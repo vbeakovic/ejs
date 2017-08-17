f4c50b1f56208fd7e1ba7a54e00bd3b7026afd91
vbeakovic <valter.beakovic@gmail.com>
2017-08-17 08:46:10 +0200

    the arguments object

--- 04_data_structures/ex_10.js
+++ 04_data_structures/ex_10.js
@@ -4,3 +4,11 @@ function noArguments() {
 noArguments(1, 2, 3);
 function threeArguments() {}
 threeArguments();
+
+
+function argumentCounter() {
+  console.log('You gave me ', arguments.length, ' arguments');
+}
+
+
+argumentCounter('Straw man', 'Tautology', 'Ad hominem');
