628bd8552574a42282555ca30ca36131315192d9
vbeakovic <valter.beakovic@gmail.com>
2017-08-17 08:49:57 +0200

    the arguments object

--- 04_data_structures/ex_10.js
+++ 04_data_structures/ex_10.js
@@ -12,3 +12,14 @@ function argumentCounter() {
 
 
 argumentCounter('Straw man', 'Tautology', 'Ad hominem');
+
+addEntry(['work', 'touched tree', 'pizza', 'running', 'television'], false);
+
+function addEntry(squirrel) {
+  var entry = {events: [], squirrel: squirrel};
+  for (var i = 1; i < arguments.length; i++) {
+    entry.events.push(arguments[i]);
+  }
+  journal.push(entry);
+}
+addEntry(true, 'work', 'touched tree', 'pizza', 'running', 'television');
