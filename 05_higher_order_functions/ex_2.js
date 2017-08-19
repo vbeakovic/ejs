419f5319e278578d955e9d90e95cac5798961a67
vbeakovic <valter.beakovic@gmail.com>
2017-08-19 17:14:57 +0200

    abstracting array traversal

--- 05_higher_order_functions/ex_2.js
+++ 05_higher_order_functions/ex_2.js
@@ -18,3 +18,35 @@ function forEach(array, action) {
   }
 }
 forEach(["Wampeter", "Foma", "Granfalloon"], console.log);
+
+var numbers = [1, 2, 3, 4, 5], sum = 0;
+forEach(numbers, function(number) {
+  sum += number;
+});
+console.log(sum);
+
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis)) {
+        phis[event] = phi(tableFor(event, journal));
+      }
+    }
+  }
+  return phis;
+}
+
+function gatherCorrelations(journal) {
+  var phis = {};
+  journal.forEach(function(entry) {
+    entry.events.forEach(function(event) {
+      if (!(event in phis)) {
+        phis[event] = phi(tableFor(event, journal));
+      }
+    });
+  });
+  return phis;
+}
