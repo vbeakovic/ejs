e41f69faf42732579454d2fc102b083fd0d403c9
vbeakovic <valter.beakovic@gmail.com>
2017-08-19 20:41:47 +0200

    filtering an array

---
+++ 05_higher_order_functions/ex_6.js
@@ -0,0 +1,15 @@
+const ANCESTRY_FILE = require("./ancestry.js");
+var ancestry = JSON.parse(ANCESTRY_FILE);
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i])) {
+      passed.push(array[i]);
+    }
+  }
+  return passed;
+}
+console.log(ancestry[1]);
+console.log(filter(ancestry, function(person) {
+  return person.born > 1900 && person.born < 1925;
+}));
